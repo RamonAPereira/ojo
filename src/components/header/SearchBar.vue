541c0d3272d799d6520095b4d78442507482eb6e
RamonAPereira <ramonpereiranzd@gmail.com>
2022-06-09 15:06:23 -0300

    feat: header

---
+++ src/components/header/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <form>
+    <input
+      class="font-light text-sm text-zinc-600 text-right w-72 p-1 rounded"
+      type="text"
+      name=""
+      id=""
+      placeholder="Pesquisar personagens"
+    />
+  </form>
+</template>
---
+++ src/layouts/header/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="bg-black">
+    <div
+      class="container mx-auto flex flex-col items-center justify-between gap-2 pt-7 pb-6 md:flex-row"
+    >
+      <h1 class="text-white font-semibold text-3xl">Star Wars Universe</h1>
+      <SearchBar />
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import SearchBar from "../../components/header/SearchBar.vue";
+</script>
